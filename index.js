166b4b40d2a4ec9cbbd9c4c98b21838fb1692e79
rin_matsuok <127410475+Fanny2712@users.noreply.github.com>
2025-02-10 08:39:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,19 @@ app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n');
+        const uName = uData[0].split('|');
+        const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) {
+            const d = uData[i].split('|');
+            tData[d[0]] = d[1];
+        }
+        if (uName[1] && uPass[1]) {
+            res.redirect('/player/growid/login/validate');
+        }
+    } catch (why) {
+        console.log(`Warning: ${why}`);
+    }
 
     res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
@@ -51,6 +60,13 @@ app.all('/player/growid/login/validate', (req, res) => {
 
     res.send(
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
+    );
+});
+
+// New endpoint added
+app.post('/player/growid/checktoken', (req, res) => {
+    res.send(
+        `{"status":"success","message":"Account Validated.","token":"${req.body.refreshToken}","url":"","accountType":"growtopia"}`,
     );
 });
 
