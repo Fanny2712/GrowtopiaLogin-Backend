56cec94736cea86380f62f5e45f6764f47e4aad6
rin_matsuok <127410475+Fanny2712@users.noreply.github.com>
2025-02-10 06:30:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -49,11 +49,9 @@ app.all('/player/growid/login/validate', (req, res) => {
         `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
 
-    app.post('/player/growid/checktoken', (req,res)=>{
     res.send(
         {"status":"success","message":"Account Validated.","token":"${req.body.refreshToken}","url":"","accountType":"growtopia"},
     );
-})
 });
 
 app.get('/', function (req, res) {
