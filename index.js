fd00e5c7ae058f8a90cf664fe1006285b7294f16
rin_matsuok <127410475+Fanny2712@users.noreply.github.com>
2025-02-10 06:19:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ app.all('/player/growid/login/validate', (req, res) => {
 })
 });
 
+app.all('/player/*', function (req, res) {
+});
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
